a7d819745902983d0a2e5334069355259a0ab380
Antoine Romand <antoine.romand@epitech.eu>
2025-08-31 00:26:27 +0200

    Changer le type de "ki" dans l'interface en string. Le parser ensuite en entier. Ajouter tout les noms des saiyans dans barChartData.labels (ngOnInit), et modifier data de datasets[0] pour ajouter les valeurs du ki de chacun.

--- src/app/main-chart/main-chart.ts
+++ src/app/main-chart/main-chart.ts
@@ -23,19 +23,25 @@ export class MainChart implements OnInit {
   public barChartType = 'bar' as const;
 
   public barChartData: ChartData<'bar'> = {
-    labels: ['2006', '2007', '2008', '2009', '2010', '2011', '2012'],
+    labels: [],
     datasets: [
-      { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' },
-      { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B' },
+      { data: [450, 500], label: 'Saiyans' },
     ],
   };
   constructor(private readonly dbzApiService: DbzApiService) { }
 
   ngOnInit(): void {
     this.dbzApiService.getSaiyanCharacters().subscribe((data) => {
-      data.forEach((character) => {
-        console.log("name: " + character.name + " // ki: " + character.ki);
+      this.barChartData.labels = data.map(d => d.name);
+      data.forEach((char) => {
+        let num = parseInt(char.ki.replace(/\./g, ""), 10);
+        console.log(num);
       });
+      this.barChartData.datasets[0].data = data.map((char) => {
+        let num = parseInt(char.ki.replace(/\./g, ""), 10);
+        return num;
+      });
+      this.chart?.update();
     });
   }
 }
