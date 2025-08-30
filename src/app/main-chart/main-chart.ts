8d5ed72d3649a783ff447597637c355b80a5f734
Antoine Romand <antoine.romand@epitech.eu>
2025-08-30 23:57:47 +0200

    Install ng2 charts et chart.js. Configuration dans app.config.ts et ajout de l'exemple de la doc.

--- src/app/main-chart/main-chart.ts
+++ src/app/main-chart/main-chart.ts
@@ -1,14 +1,34 @@
-import { Component, type OnInit } from '@angular/core';
+import { Component, ViewChild, type OnInit } from '@angular/core';
 import { DbzApiService } from '../services/dbz-api.service';
+import { BaseChartDirective } from 'ng2-charts';
+import type { ChartConfiguration, ChartData } from 'chart.js';
 
 @Component({
   selector: 'app-main-chart',
-  imports: [],
+  imports: [BaseChartDirective],
   templateUrl: './main-chart.html',
   styleUrl: './main-chart.css',
   providers: [DbzApiService]
 })
 export class MainChart implements OnInit {
+  @ViewChild(BaseChartDirective) chart?: BaseChartDirective<'bar'>;
+
+  public barChartOptions: ChartConfiguration<'bar'>['options'] = {
+    responsive: true,
+    maintainAspectRatio: false,
+    scales: { x: {}, y: { min: 10 } },
+    plugins: { legend: { display: true } },
+  };
+
+  public barChartType = 'bar' as const;
+
+  public barChartData: ChartData<'bar'> = {
+    labels: ['2006', '2007', '2008', '2009', '2010', '2011', '2012'],
+    datasets: [
+      { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' },
+      { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B' },
+    ],
+  };
   constructor(private readonly dbzApiService: DbzApiService) { }
 
   ngOnInit(): void {
@@ -19,3 +39,4 @@ export class MainChart implements OnInit {
     });
   }
 }
+
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
+import { provideCharts, withDefaultRegisterables } from 'ng2-charts';
+import { Chart as ChartJS, registerables } from 'chart.js';
 
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
+
+ChartJS.register(...registerables);
